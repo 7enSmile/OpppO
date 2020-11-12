25242efa183afeaae3cc8812136a822ffcc253df
7enSmile <68625468+7enSmile@users.noreply.github.com>
2020-11-12 17:31:31 +0700

    Исправление бага на отрицательную степень

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 double my_pow(double base, unsigned degree) {
 	double count = 1;
-	for (int i = 0; i < degree; i++) {
-		count = count * degree;
+	if (degree == 0) return 1;
+	if (degree > 0) {
+		for (int i = 0; i < degree; i++) {
+			count = count * degree;
+		}
+	}
+	else {
+		degree = degree * -1;
+		for (int i = 0; i < degree; i++) {
+			count = count * degree;
+		}
+		count = count / 10;
+
 	}
 	return count;
 
