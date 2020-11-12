68216e30f44dc1182a80474b8de63f6439caf639
7enSmile <68625468+7enSmile@users.noreply.github.com>
2020-11-12 17:59:11 +0700

    исправление фикса

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 double my_pow(double base, unsigned degree) {
 	double count = 1;
-	if (degree == 0) return 1;
-	if (degree > 0) {
-		for (int i = 0; i < degree; i++) {
-			count = count * degree;
-		}
-	}
-	else {
-		degree = degree * -1;
-		for (int i = 0; i < degree; i++) {
-			count = count * degree;
-		}
-		count = count / 10;
+	bool flag = 0;
 
+	if (degree == 0) return 1;
+	if (degree < 0) {
+		degre = degree * -1;
+		flag = 1;
 	}
-	return count;
+
+	for (int i = 0; i < degree; i++) {
+		count = count * degree;
+	}
+
+	if (flag) {
+		return count;
+	}
+	else
+	{
+		return 1 / count;
+	}
 
 }
